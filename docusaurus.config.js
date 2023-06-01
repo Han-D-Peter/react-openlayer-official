b6fced38b8e7a9d18477adffd8fb792a7f222db1
Han-D-Peter <secret_wings@naver.com>
2023-06-01 09:08:48 +0900

    chore: 배포 셋팅

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Han-D-Peter', // Usually your GitHub org/user name.
+  projectName: 'react-openlayer-official', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
