808a0a7476e342ce19f64de1770db11b5853b087
Han-D-Peter <secret_wings@naver.com>
2024-09-11 13:25:12 +0900

    feat: new feature

--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -39,6 +39,8 @@ import {
   icon,
   ControlGroup,
   ImageOveray,
+  ImageMapForPixel,
+  BoundaryCircle,
 } from "react-openlayers7";
 // Add react-live imports you need here
 
@@ -84,8 +86,29 @@ const ReactLiveScope = {
   icon,
   ControlGroup,
   ImageOveray,
+  ImageMapForPixel,
+  BoundaryCircle,
 };
 
 ReactLiveScope.icon.marker = "/img/marker-basic.png";
 ReactLiveScope.icon.selected = "/img/marker-selected.png";
+ReactLiveScope.icon.imageMarker.selected = "/img/imageMarker(selected).png";
+ReactLiveScope.icon.imageMarker.zero = "/img/imageMarker(one).png";
+ReactLiveScope.icon.imageMarker.one = "/img/imageMarker(two).png";
+ReactLiveScope.icon.imageMarker.two = "/img/imageMarker(three).png";
+ReactLiveScope.icon.imageMarker.three = "/img/imageMarker(four).png";
+ReactLiveScope.icon.imageCircleMarker.BLUE = "/img/imageCircleMarker/BLUE.png";
+ReactLiveScope.icon.imageCircleMarker.BROWN =
+  "/img/imageCircleMarker/BROWN.png";
+ReactLiveScope.icon.imageCircleMarker.GREEN =
+  "/img/imageCircleMarker/GREEN.png";
+ReactLiveScope.icon.imageCircleMarker.RED = "/img/imageCircleMarker/RED.png";
+ReactLiveScope.icon.imageCircleMarker.ROYAL_BLUE =
+  "/img/imageCircleMarker/ROYAL_BLUE.png";
+ReactLiveScope.icon.imageCircleMarker.SELECT =
+  "/img/imageCircleMarker/SELECT.png";
+ReactLiveScope.icon.imageCircleMarker.SKYBLUE =
+  "/img/imageCircleMarker/SKYBLUE.png";
+ReactLiveScope.icon.imageCircleMarker.YELLOW =
+  "/img/imageCircleMarker/YELLOW.png";
 export default ReactLiveScope;
